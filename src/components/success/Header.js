5338ff89b3aa8cf04289481ca4fc69a73d763581
engradilhameed <adilhameed200@gmail.com>
2023-05-28 02:40:24 +0500

    button issue

--- src/components/success/Header.js
+++ src/components/success/Header.js
@@ -9,7 +9,7 @@ export default function Header() {
                     <h1 className='fw-bolder'>
                         You just <span style={{ color: "#40FF62" }}>Rocked</span> that form, thanks!
                     </h1>
-                    <p className='d-inline-block light-green p-2 px-4 mb-0 fw-semibold fs-22 rounded roboto'>
+                    <p className='d-inline-block light-green p-2 px-4 mb-0 fw-semibold fs-22 rounded roboto mt-lg-0 mt-3'>
                         Book A Call
                     </p>
                 </div>
